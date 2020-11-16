225655a121312bd3af7b2dd567528311f5315a84
TheJapsu1 <japsu.honkasalo@gmail.com>
2020-11-16 17:01:15 +0200

    ma 16.11.2020 17.01.15,31

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System;
+
+public class CustomNetworkManager : NetworkManager
+{
+    public override void OnStartServer()
+    {
+        Debug.Log($"Server started ({DateTime.Now}).");
+    }
+
+    public override void OnStopServer()
+    {
+        Debug.Log($"Server stopped ({DateTime.Now}).");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log($"Client connected with the address '{conn.address}' and ID '{conn.connectionId}'.");
+
+        base.OnClientConnect(conn);
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        Debug.Log($"Client disconnected with the address '{conn.address}' and ID '{conn.connectionId}'.");
+
+        base.OnClientDisconnect(conn);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 
 public class PlayerController : NetworkBehaviour
 {
+    [SyncVar(hook = nameof(OnTestCountChanged))]
+    int testCount = 0;
+
     public float speed = 0.1f;
 
     private void Update()
     {
         HandleMovement();
+
+        if(isLocalPlayer && Input.GetKeyDown(KeyCode.T))
+        {
+            //run on client
+            Debug.Log($"Sending a 'Test' command.");
+            Test();
+        }
+
+        if(isServer && transform.position.y > 5.5f)
+        {
+            //never spam commands through network lmao
+            TooHigh();
+        }
     }
 
     public void HandleMovement()
@@ -22,5 +38,38 @@ public class PlayerController : NetworkBehaviour
 
             transform.position = transform.position + movement;
         }
+    }
+
+    public override void OnStartServer()
+    {
+        Debug.Log($"Player has been spawned on the server.");
+    }
+
+    [Command]
+    private void Test()
+    {
+        //run on server
+        Debug.Log($"Received a 'Test' command from client.");
+        testCount ++;
+        ReceivedTest();
+    }
+
+    [TargetRpc]
+    private void ReceivedTest()
+    {
+        //run on client
+        Debug.Log($"Received a 'Test' command from the server.");
+    }
+
+    [ClientRpc]
+    private void TooHigh()
+    {
+        //run on client
+        Debug.Log($"Too high!");
+    }
+
+    void OnTestCountChanged(int oldCount, int newCount)
+    {
+        Debug.Log($"Incremented Tests by one ({oldCount} -> {newCount}).");
     }
 }
