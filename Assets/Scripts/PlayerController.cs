db4fd3f67a03da6ed946927bf5adcea316271293
TheJapsu1 <japsu.honkasalo@gmail.com>
2020-11-16 12:33:27 +0200

    Basic networking

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour
+{
+    public float speed = 0.1f;
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    public void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float inputHorizontal = Input.GetAxis("Horizontal");
+            float inputVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(inputHorizontal * speed, inputVertical * speed, 0);
+
+            transform.position = transform.position + movement;
+        }
+    }
+}
